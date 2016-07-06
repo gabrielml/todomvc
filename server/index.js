c3137cf1d2e533fb1fa0c61f2cfd3b19357a9f50
Gabriel Mijares <gabrielmljob@gmail.com>
2016-07-06 14:49:47 +0200

    (refactor): simplify server code

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
-var app = express();
 
 var PORT = process.env.PORT || 3000;
 
-app.use(express.static('client'));
-app.listen(PORT, function() {
+express()
+    .use(express.static('client'))
+    .listen(PORT, function() {
     console.log('application running on port ' + PORT);
 });
