cd5cc7cb888b4d63f5bd081104563233adb82a2f
Gabriel Mijares <gabrielmljob@gmail.com>
2016-07-06 14:48:04 +0200

    (refactor): separate client and server files

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ var app = express();
 
 var PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname));
+app.use(express.static('client'));
 app.listen(PORT, function() {
     console.log('application running on port ' + PORT);
 });
